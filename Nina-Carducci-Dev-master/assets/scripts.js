e7da0cffe3ad6db3c2148d8896f74516a34ee55a
armadne <madane.aroul@hotmail.com>
2025-09-12 13:53:08 +0200

    modification du code scripts.js (application de jquery) et ajustement du constraste pour le filtre "Tous"

--- Nina-Carducci-Dev-master/assets/scripts.js
+++ Nina-Carducci-Dev-master/assets/scripts.js
@@ -1,12 +1,6 @@
 document.addEventListener("DOMContentLoaded", function () {
-  new MauGallery('.gallery', {
-    columns: {
-      xs: 1,
-      sm: 2,
-      md: 3,
-      lg: 3,
-      xl: 3
-    },
+  $('.gallery').mauGallery({
+    columns: { xs: 1, sm: 2, md: 3, lg: 3, xl: 3 },
     lightBox: true,
     lightboxId: 'myAwesomeLightbox',
     showTags: true,
